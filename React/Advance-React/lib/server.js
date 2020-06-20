f5f889adaf337024081df9ce3809b182cb7de830
Gagan Deep Sharma <luck.gagan@gmail.com>
2020-06-19 23:42:45 -0400

    Config changes

--- React/Advance-React/lib/server.js
+++ React/Advance-React/lib/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const config = require("./config");
 
 const app = express();
 
 app.use(express.static("public"));
 
-app.listen(config.port);
+app.listen(config.port),
+  function listenHandler() {
+    console.log(`Running on ${config.port}`);
+  };
