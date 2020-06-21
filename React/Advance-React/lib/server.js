88c32713e0ad79e5c1674a365d88e3f9431bb5b3
Gagan Deep Sharma <luck.gagan@gmail.com>
2020-06-20 23:32:48 -0400

    Fixed eslint issues

--- React/Advance-React/lib/server.js
+++ React/Advance-React/lib/server.js
@@ -1,11 +1,18 @@
-const express = require("express");
-const config = require("./config");
+const express = require('express');
+
+const config = require('./config');
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.listen(config.port),
-  function listenHandler() {
-    console.log(`Running on ${config.port}`);
-  };
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index', { answer: 42 });
+});
+
+app.listen(config.port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Running on ${config.port}`);
+});
