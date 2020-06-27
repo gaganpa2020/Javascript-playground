d0332b9d002b6e4f81a336eaa0d0a0133edc2b40
Gagan Deep Sharma <luck.gagan@gmail.com>
2020-06-26 22:57:29 -0400

    Fixed UI issues.

--- React/Advance-React/lib/components/Article.js
+++ React/Advance-React/lib/components/Article.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const Article = (props) => {
   const { article, author } = props;
+
   return (
     <div>
       <div>{article.title}</div>
--- React/Advance-React/lib/components/ArticleList.js
+++ React/Advance-React/lib/components/ArticleList.js
@@ -4,14 +4,13 @@ import Article from './Article';
 const ArticleList = (props) => {
   return (
     <div>
-      {Object.values(props.articles).map((article) => {
-        debugger;
+      {Object.values(props.articles).map((article) => (
         <Article
           key={article.id}
           article={article}
           author={props.authors[article.authorId]}
-        ></Article>;
-      })}
+        />
+      ))}
     </div>
   );
 };
